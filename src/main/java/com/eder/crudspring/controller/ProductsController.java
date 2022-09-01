f90d3adb1c2b5a59b79f967cdfa139891dc15f11
Eder-R <eder31250@gmail.com>
2022-09-01 10:46:26 -0300

    Database H2 created with success

--- src/main/java/com/eder/crudspring/controller/ProductsController.java
+++ src/main/java/com/eder/crudspring/controller/ProductsController.java
@@ -2,8 +2,13 @@ package com.eder.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eder.crudspring.model.Product;
@@ -12,15 +17,24 @@ import com.eder.crudspring.repo.ProductRepo;
 import lombok.AllArgsConstructor;
 
 @RestController
-@RequestMapping("/api/projects")
+@RequestMapping("/api/products")
 @AllArgsConstructor
 public class ProductsController {
 
   private final ProductRepo productRepo;
   
   @GetMapping
-  public List<Product> list() {
+  public @ResponseBody List<Product> list() {
 
     return productRepo.findAll();
   }
+
+  @PostMapping
+  @ResponseStatus(code = HttpStatus.CREATED)
+  public Product create(@RequestBody Product product){
+    
+    return productRepo.save(product);
+
+  }
+
 }
